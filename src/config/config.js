24cccc3b1e0d35826a43c2cab6f6f3f81e6e620c
Finn Terdal <fterdal@gmail.com>
2018-12-13 18:23:33 -0600

    fixes ground by adding physics

--- src/config/config.js
+++ src/config/config.js
@@ -5,9 +5,16 @@ export default {
   width: 800,   // Game width in pixels
   height: 600,  // Game height in pixels
   // physics: { ... },   // Will be explained in more detail later
+  physics: {    // Optional: specify physics engine and configuration
+    default: 'arcade',  // A simple and performant physics engine
+    arcade: {
+      gravity: { y: 1500 },  // Game objects will by default be affected by gravity
+      debug: false,
+    },
+  },
   // This option is to turn off the default behavior of images being automatically sharpened.
   // Since we'll be using pixel art, we want every beautiful pixel untouched!
   render: {
     pixelArt: true,
   }
-};
+}
--- src/scenes/FgScene.js
+++ src/scenes/FgScene.js
@@ -1,6 +1,6 @@
 /* global Phaser */
-import Player from '../entity/Player'
-import Ground from '../entity/Ground'
+import Player from '../entity/Player';
+import Ground from '../entity/Ground';
 
 export default class FgScene extends Phaser.Scene {
   constructor() {
@@ -8,63 +8,35 @@ export default class FgScene extends Phaser.Scene {
   }
 
   preload() {
-    // Preload Sprites
-    // << LOAD SPRITES HERE >>
-    // this.load.path = 'assets/'
+    // Sprites
+    this.load.image('ground', 'assets/sprites/ground.png');
     this.load.spritesheet('josh', 'assets/spriteSheets/josh.png', {
       frameWidth: 340,
       frameHeight: 460,
-    })
-    this.load.image('ground', 'assets/sprites/ground.png')
-    // this.load.spritesheet('josh')
+    });
 
-    // Preload Sounds
-    // << LOAD SOUNDS HERE >>
   }
 
   create() {
-    // Create game entities
-    // << CREATE GAME ENTITIES HERE >>
-    this.player = new Player(this, 80, 400, 'josh').setScale(0.25)
-    // this.ground = new Ground(this, 80, 400, 'ground')
-    // this.groundGroup = this.physics.add.staticGroup({ classType: Ground });
-    this.createGroups(160, 540);
-    // this.createGround(600, 540);
+    // Create the ground and lasers
+    this.createGroups();
 
+    // Josh. The player. Our sprite is a little large, so we'll scale it down
+    this.player = new Player(this, 20, 400, 'josh').setScale(0.25);
 
-    // Create sounds
-    // << CREATE SOUNDS HERE >>
-
-    // Create collisions for all entities
-    // << CREATE COLLISIONS HERE >>
   }
 
+  // Make the ground
   createGround(x, y) {
     this.groundGroup.create(x, y, 'ground');
   }
 
+  // Make all the groups
   createGroups() {
     this.groundGroup = this.physics.add.staticGroup({ classType: Ground });
     //add ground to group
     this.createGround(160, 540);
     this.createGround(600, 540);
-    this.lasers = this.physics.add.group({
-      classType: Laser,
-      maxSize: 40,
-      runChildUpdate: true,
-      allowGravity: false
-    });
-  }
-
-  // createGround(x, y) {
-  //   console.log('CREATING Groups')
-  //   this.groundGroup.create(x, y, 'ground');
-  // }
-
-  // time: total time elapsed (ms)
-  // delta: time elapsed (ms) since last update() call. 16.666 ms @ 60fps
-  update(time, delta) {
-    // << DO UPDATE LOGIC HERE >>
   }
 
 }
